42a3d8799eb73939b189478d9356ced08311565f
xjjdj <68924238+xjjdj@users.noreply.github.com>
2021-03-21 03:01:43 +0800

    ddd
    dd

--- server.js
+++ server.js
@@ -36,7 +36,12 @@ module.exports = function createServer() {
   server.listen(80, function () {
     console.log("Server started on port 80")
   })
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  return next();
+});
   app.use(morgan('dev'))
 
   app.get('/', function (req, res) {
